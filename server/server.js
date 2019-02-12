04c5e488c4246ac67348ae78d9c9280b61e58151
Julia <Julia@Julias-MacBook-Pro.local>
2019-02-11 22:18:02 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}!`);
+});
