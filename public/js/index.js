bc54ec5277fecdcb8fbade69a7365eed55a105b0
Julia <Julia@Julias-MacBook-Pro.local>
2019-02-12 13:08:38 -0500

    Added welcomeMessage for new user and notification for existing chat app users

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,14 @@ socket.on('connect', function () {
         console.log('Message received!', messageToClient);
     });
 
+    socket.on('adminMessage', function (joinedServer) {
+        console.log('User joined chatroom!', joinedServer);
+    });
+
+    socket.on('welcomeMessage', function (greeting) {
+        console.log(greeting);
+    });
+
     socket.on('disconnect', function () {
         console.log('Disconnected from server.');
     });
--- server/server.js
+++ server/server.js
@@ -17,6 +17,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('A new user connected!');
 
+    socket.emit('welcomeMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('adminMessage', {
+        from: 'Admin',
+        text: 'A new user joined!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -24,6 +36,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     // socket disconnect.
