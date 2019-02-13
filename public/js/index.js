d40059f2a5e070a5d0b0d0a711c532032dea944c
Julia <Julia@Julias-MacBook-Pro.local>
2019-02-13 09:33:02 -0500

    Added css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function (message) {
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = $('[name=message]')
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('')
     });
 });
 
@@ -42,13 +44,17 @@ var locationButton = $('#send-location');
 locationButton.click(function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
-    } else {
+    }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+    {
         navigator.geolocation.getCurrentPosition(function (position) {
+            locationButton.removeAttr('disabled').text('Send location');
             socket.emit('createLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
         }, function () {
+            locationButton.removeAttr('disabled').text('Send location');
             alert('Unable to fetch location.');
         });
     };
