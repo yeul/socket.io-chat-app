19c15e487b0b2150439188432f30d7469585e8e3
Julia <Julia@Julias-MacBook-Pro.local>
2019-02-13 15:18:55 -0500

    Added autoscrolling if user is close to bottom of chat window

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', function () {
     console.log('Connected to server.');
 
@@ -13,11 +29,8 @@ socket.on('connect', function () {
         });
 
         $('#messages').append(html);
-        // console.log('Message received!', message);
-        // var li = $('<li></li>');
-        // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+        scrollToBottom();
 
-        // $('#messages').append(li);
     });
 
     socket.on('disconnect', function () {
@@ -35,14 +48,8 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 
-    // var li = $('<li></li>');
-    // var a = $('<a target="_blank">My current location</a>');
-
-    // li.text(`${message.from} ${formattedTime}: `);
-    // a.attr('href', message.url)
-    // li.append(a);
-    // $('#messages').append(li);
 });
 
 $('#message-form').on('submit', function (e) {
@@ -65,16 +72,15 @@ locationButton.click(function () {
         return alert('Geolocation not supported by your browser.');
     }
     locationButton.attr('disabled', 'disabled').text('Sending location...');
-    {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            locationButton.removeAttr('disabled').text('Send location');
-            socket.emit('createLocationMessage', {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            });
-        }, function () {
-            locationButton.removeAttr('disabled').text('Send location');
-            alert('Unable to fetch location.');
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
         });
-    };
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location.');
+    });
+
 });
